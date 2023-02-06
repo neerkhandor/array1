580d6c960dc092e50b19edf47cb1612b7284b2ca
neerkhandor <82304249+neerkhandor@users.noreply.github.com>
2023-02-06 14:09:29 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@array1/main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+void printArray(int arr[],int n){
+  for(int i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+  }
+}
+//reverse array
+void reverseArray(int arr[],int n){
+  int start=0;
+  int end=n-1;
+  while(start<end){
+    swap(arr[start],arr[end]);
+    start++;
+    end--;
+  }
+}
+
+int maximum(int arr[],int n){
+  int maxi=INT8_MIN;
+  for(int i=0;i<n;i++){
+    if(arr[i]>maxi){
+      maxi=arr[i];
+    }
+  }
+  return maxi;
+}
+int minimum(int arr[],int n){
+  int mini=INT8_MAX;
+  for(int i=0;i<n;i++){
+    if(arr[i]<mini){
+      mini=arr[i];
+    }
+  }
+  return mini;
+}
+
+int linearSearch(int arr[],int n,int key){
+  for(int i=0;i<n;i++){
+    if(arr[i]==key){
+      return i;
+    }
+  }
+  return -1;
+}
+int main() {
+int arr[]={10,11,12,13,14};
+int arr1[]={15,16,17,18};
+int n=sizeof(arr)/sizeof(arr[0]);
+int n1=sizeof(arr1)/sizeof(arr[0]);
+ // cout<<n1;
+  // int n=sizeof(arr)/sizeof(int);    //both are same things
+  // cout<<n;
+// printArray(arr, n);
+// cout<<endl;
+// printArray(arr1,n1);
+  // printArray(arr, n);
+  // cout<<endl;
+  // reverseArray(arr,n);
+  // cout<<endl;
+  // printArray(arr, n);
+  // cout<<endl;
+  // printArray(arr1, n1);
+  // cout<<endl;
+  // reverseArray(arr1,n1);
+  // cout<<endl;
+  // printArray(arr1, n1);
+
+  // int max=maximum(arr, n);
+  // int min=minimum(arr, n);
+  // cout<<"Maximmum element in the array is "<<max<<endl;
+  // cout<<"Minimum element in the array is "<<min<<endl;
+  int key;
+  cin>>key;
+  int ans=linearSearch(arr, n, key);
+  cout<<ans<<endl;
+
+  
+
+  
+}
